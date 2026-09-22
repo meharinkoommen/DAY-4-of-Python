c630daa3b4d5a480ad6ae1f09a4ee61c8c66efe2
meharinkoommen <meharinkoommen@gmail.com>
2026-09-22 17:52:22 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,66 @@
+
+
+
+
+
+#reading string from the user
+
+student_name=input("Enter the student name")
+print(student_name)
+print(type(student_name))
+
+#reading number from the user
+student_age=int(input("Enter the student age"))
+print(student_age)
+print(type(student_age))
+
+#reading float from the user
+student_marks=float(input("Enter the student marks"))
+print(student_marks)
+print(type(student_marks))
+
+'''
+
+write a program on the below topics
+
+1.studentname
+2.fathername
+3.pincode
+4.intermarks
+
+'''
+
+
+
+student_name=input("Enter the student name")
+print(student_name)
+print(type(student_name))
+
+father_name=input("Enter the father name")
+print(father_name)
+print(type(father_name))
+
+
+pincode=int(input("Enter the pincode"))
+print(pincode)
+print(type(pincode))
+
+inter_marks=float(input("Enter the intermediate marks"))
+print(inter_marks)
+print(type(inter_marks))
+
+
+print("Student Name:", student_name)
+print(f"student name is {student_name}")
+print("Father Name:", father_name)
+print(f"Father Name: {father_name}")
+print("Pin Code:", pincode)
+print(f"Pin Code: {pincode}")
+print("Intermediate Marks:", inter_marks)
+print(f"Intermediate Marks: {inter_marks}")
+
+'''
+input()--->string
+int(input())--->integer
+float(input())--->float
+'''
